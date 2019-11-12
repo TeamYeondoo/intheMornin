a46fa8d1733f06fa8c0f5505cc4447755752623c
student <veroroot@gmail.com>
2019-11-12 10:35:43 +0900

    function related Customer

--- intheMornin/src/main/java/com/inthemornin/ootd/controller/CustomerController.java
+++ intheMornin/src/main/java/com/inthemornin/ootd/controller/CustomerController.java
@@ -4,28 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.inthemornin.ootd.service.IService;
 
 @Controller
-public class ClothController {
+public class CustomerController {
 	
 	@Autowired
-	IService clothService;
+	IService customerService;
 	
-	@RequestMapping(value="/hr/clothcount") // URL주소 뒤에 해당 사이트에 요청을 보내라
-	public String clothCount(
+	@RequestMapping(value="/hr/custcount") // URL주소 뒤에 해당 사이트에 요청을 보내라
+	public String customerCount(
 		@RequestParam(value="deptid", required=false, defaultValue="0") 
 		int deptid, Model model) {
 		if(deptid==0) {
-			model.addAttribute("clothcount", clothService.getCount());
+			model.addAttribute("custcount", customerService.getCount());
 		}else {
-			model.addAttribute("clothcount", clothService.getCount(deptid));
+			model.addAttribute("custcount", customerService.getCount(deptid));
 		}
-		return "hr/clothcount";
+		return "hr/custcount";
 	}
 	
-
-
+	
 }
